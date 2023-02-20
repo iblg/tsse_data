ab2c00492932bdad5091ae984537b45bb5be4789
Ian Billinge <ianbillinge@Ians-MacBook-Pro-8.local>
2023-02-20 13:27:07 -0500

    Fixed error handling for common_dims parameter.

--- tsse_data/toc_measurement.py
+++ tsse_data/toc_measurement.py
@@ -108,15 +108,15 @@ def process_toc_spreadsheet(filepath, dims, common_dims = None):
     if common_dims is None:
         pass
     elif isinstance(common_dims, dict):
-        pass
+        for key, val in common_dims.items():  # write one column per common_dim
+            df[key] = val
     else:
         print('\n \n \ncommon_dims was passed but was format {}'.format(type(common_dims)))
         print('common_dims must be a dict. \n \n \n ')
 
 
 
-    for key, val in common_dims.items(): #write one column per common_dim
-        df[key] = val
+
 
     idx = dims
     [idx.append(d) for d in common_dims]
