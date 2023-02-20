ceb5dc1259d11ddc6ea008fbd9c8b4a3502ab97a
Ian Billinge <ianbillinge@Ians-MacBook-Pro-8.local>
2023-02-20 13:52:29 -0500

    Added a process_kf_measurement function.

--- tsse_data/kf_measurement.py
+++ tsse_data/kf_measurement.py
@@ -27,7 +27,53 @@ def create_kf_spreadsheet(filepath, dims):
 def read_kf_spreadsheet(filepath):
     return pd.read_excel(filepath)
 
+def process_kf_spreadsheet(filepath, dims, common_dims = None):
+    """
+    filepath : str
+    The filepath to the TOC spreadsheet you wish to create.
 
+    dims : list or array
+    The list of column names that you wish to use as dims. These should be the same as the dims of your overall experiment.
+
+    common_dims : dict, default None.
+    additional dims that apply to all measurements in the spreadsheet. Keys become xarray dims; vals become xarray coords.
+    """
+    df = read_toc_spreadsheet(filepath)
+
+    if isinstance(filepath, str):
+        pass
+    else:
+        print('\n \n \nfilepath was passed but was format {}'.format(type(filepath)))
+        print('filepath must be a string. \n \n \n ')
+
+    if isinstance(dims, list):
+        idx = dims
+
+        pass
+    else:
+        print('\n \n \ndims was passed but was format {}'.format(type(dims)))
+        print('dims must be a list of strings. \n \n \n ')
+
+    if common_dims is None:
+        pass
+    elif isinstance(common_dims, dict):
+        for key, val in common_dims.items():  # write one column per common_dim
+            df[key] = val
+        [idx.append(d) for d in common_dims]
+
+    else:
+        print('\n \n \ncommon_dims was passed but was format {}'.format(type(common_dims)))
+        print('common_dims must be a dict. \n \n \n ')
+
+    df = df.set_index(idx)
+    df = adjust_for_dilution(df)
+    ds = df.to_xarray()
+
+    for i in idx:
+        ds = ds.drop_duplicates(dim = i)
+    # df = convert_toc_w_amine(df, nc, mw)
+
+    return ds
 
 
 def main():
