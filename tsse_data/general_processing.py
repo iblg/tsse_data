Fixed error in error calculation. Now errors match the spreadsheets as well.

tsse_data/general_processing.py
@@ -71,7 +71,7 @@ def average_over_replicates(ds, ions, salt_conversion):
     for ion, calibration in ions.items():
         # average over replicates
         ds['w_' + ion] = ds['w_' + ion + '_rep'].mean(dim='replicate')
-        ds['dw_' + ion] = ds['w_' + ion + '_rep'].std(dim='replicate') / np.sqrt(ds.sizes['replicate'])
+        ds['dw_' + ion] = ds['w_' + ion + '_rep'].std(dim='replicate')
 
         if salt_conversion is not None:
             ds = convert_ion_to_salt(ds, salt_conversion)

tsse_data/kf_measurement.py
@@ -66,20 +66,19 @@ def find_ww(ds):
     x = ds['wt_percent_water'] / 100.
 
     ds['w_w'] = x.mean(dim='replicate')
-    ds['dw_w'] = x.std(dim='replicate') / np.sqrt(2)
+    ds['dw_w'] = x.std(dim='replicate')
 
     return ds
 
 
 def main():
-    dims = ['amine', 'sample', 'phase']
+    dims = ['sample', 'replicate']
     fp = './kf_sheet.xlsx'
-    create_kf_spreadsheet(fp, dims)
-    # fp = '/Users/ianbillinge/Documents/yiplab/projects/new_saxs/phase_diagram/pd_kf.xlsx'
-    # df = read_kf_spreadsheet(fp)
-    # dims = ['amine', 'temperature', 'sample', 'replicate']
-    # ds = process_kf_spreadsheet(fp, dims, common_dims={'phase': 'org'})
-    # print(ds)
+    # create_kf_spreadsheet(fp, dims)
+    # fp = '/Users/ianbillinge/dev/tsse_data/tsse_data/tests/merge_tests/kf.xlsx'
+    df = read_kf_spreadsheet(fp)
+    ds = process_kf_spreadsheet(fp, dims, common_dims={'phase': 'org'})
+    print(ds)
 
     return
 